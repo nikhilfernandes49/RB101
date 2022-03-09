6f1cbe7cb93ed3d7a0b5cfe42d1a0b5f7f8f2f12
Nikhil Fernandes <Nikhilfernandes49@gmail.com>
2022-03-09 08:12:20 +0530

    Adding the mortgage calculator

--- lesson2/mortgage.rb
+++ lesson2/mortgage.rb
@@ -1,44 +1,84 @@
-=begin
-Build a car mortgage calculator
-You will need:
-1) loan amount
-2) annual percentage rate (APR)
-3) loan duration
+require "yaml"
+MESSAGES = YAML.load_file('mortgage_messages.yml')
 
-Calculate the following:
-1) monthly interest rate
-2) loan duration in months
-3) monthly payment
+def prompt(key)
+  message = (MESSAGES[key])
+  puts "=> #{message}"
+end
 
-m = p * (j / (1 - (1 + j)**(-n)))
-m = monthly payment
-p = loan amount
-j = monthly interest rate
-n = loan duration in months
+def valid_name?(string)
+  string.to_i == 0
+end
 
-=end
+def valid_integer(input)
+  input.to_i.to_s == input
+end
 
+def valid_float(input)
+  input.to_f.to_s == input
+end
 
+def valid_number?(number)
+  valid_integer(number) || valid_float(number)
+end
 
+prompt('welcome')
+name = ""
+loop do
+  name = gets.chomp
+  if valid_name?(name)
+    break
+  else
+    prompt('wrong_value')
+  end
+end
 
+puts "=> Hi, #{name.capitalize}!"
 
+loop do
+  prompt('amount')
+  loan_amount = ""
+  loop do
+    loan_amount = gets.chomp
+    if valid_number?(loan_amount)
+      loan_amount = loan_amount.to_f
+      break
+    else
+      prompt('wrong_value')
+    end
+  end
 
+  prompt('apr')
+  apr = ""
+  loop do
+    apr = gets.chomp
+    if valid_number?(apr)
+      apr = apr.to_f
+      break
+    else
+      prompt('wrong_value')
+    end
+  end
+  apr /= 100
+  monthly_rate = apr / 12
 
+  prompt('duration')
+  loan_duration = ""
+  loop do
+    loan_duration = gets.chomp
+    if valid_number?(loan_duration)
+      loan_duration = loan_duration.to_f
+      break
+    else
+      prompt('wrong_value')
+    end
+  end
+  loan_duration *= 12
 
+  monthly_payment = (loan_amount * (monthly_rate / (1 - (1 + monthly_rate)**(-loan_duration)))).round(2)
+  puts "The monthly payment you have to make is $#{monthly_payment}"
 
-puts "=> Please enter the loan amount"
-loan_amount = gets.chomp.to_f
-puts loan_amount
-
-puts "=> Please enter the annual percentage rate"
-apr = gets.chomp.to_f
-monthly_rate = apr/12
-puts monthly_rate
-
-puts "=> Please enter the loan duration"
-loan_duration = gets.chomp.to_f
-loan_duration = loan_duration*12
-puts loan_duration
-
-monthly_payment = loan_amount * (monthly_rate / (1 - (1 + monthly_rate)**(-loan_duration)))
-p monthly_payment
+  prompt('again?')
+  again = gets.chomp.downcase
+  break unless again.start_with?('y')
+end
